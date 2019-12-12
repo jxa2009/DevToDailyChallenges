8d908ebb5c3ff028893cfc463d01ba38e0a8f3ef
Jason Au <jxa2009@g.rit.edu>
2019-12-12 17:00:29 -0500

    Daily Challenge # 135 - 'The Wide Mouthed Frog!' - Solution - dev.to

---
+++ 135/main.py
@@ -0,0 +1,12 @@
+# Frog loves asking creatures he encoutners what they like to eat.
+# Meets the alligator who loves to eat wide-mouthed frogs!
+# When he meets the alligator, it makes a tiny mouth.
+
+# Goal is to create complete the mouth_size method this method takes one argumenta nimal which corresponds to the animal encountered by the frog.
+# If this one is an alligator (case insensitive) return small otherwise return wide.
+
+class Solution(object):
+    def mouth_size(self, animal)
+        if animal == "alligator":
+            return "small"
+        return "wide"
